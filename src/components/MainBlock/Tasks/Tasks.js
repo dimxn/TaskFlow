eb5e33e3901ee3d8f6e02c445ca28bcdf1a26a06
dimxn <zakermen.d@gmail.com>
2023-06-20 16:25:21 +0300

    Fix class

--- src/components/MainBlock/Tasks/Tasks.js
+++ src/components/MainBlock/Tasks/Tasks.js
@@ -134,7 +134,6 @@ export const Tasks = ({titlePage, showCompleted, userId}) => {
                 <ShowTask
                     setShowTask={setShowTask}
                     selectedTask={selectedTask}
-
                 />
             )}
         </section>
